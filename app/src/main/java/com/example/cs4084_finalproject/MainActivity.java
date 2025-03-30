bbb298e15dda75514b5e47daf0b6637f37097ff3
LORCAN HYLAND <21340609@studentmail.ul.ie>
2025-03-30 17:22:36 +0100

    Added and configured BottomNavigationView with fragment switching and added app icons

--- app/src/main/java/com/example/cs4084_finalproject/MainActivity.java
+++ app/src/main/java/com/example/cs4084_finalproject/MainActivity.java
@@ -1,24 +1,60 @@
 package com.example.cs4084_finalproject;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.example.cs4084_finalproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Sets default fragment on startup
+        replaceFragment(new HomeFragment());
+
+        // Make BottomNavigationView background transparent
+        binding.bottomNavigationView.setBackgroundResource(android.R.color.transparent);
+
+        // BottomNavigationView listener
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.home) {
+                selectedFragment = new HomeFragment();
+            } else if (itemId == R.id.jokes) {
+                selectedFragment = new JokeFragment();
+            } else if (itemId == R.id.memes) {
+                selectedFragment = new MemeFragment();
+            } else if (itemId == R.id.account) {
+                selectedFragment = new AccountFragment();
+            }
+
+            if (selectedFragment != null) {
+                replaceFragment(selectedFragment);
+            }
+
+            return true;
         });
+
+        // Upload button (island-style FAB)
+        binding.fabUpload.setOnClickListener(v -> {
+            replaceFragment(new UploadFragment());
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
