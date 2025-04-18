47fd6ac54d01c06b9f4c4e1d2b040614ca5a10da
Vivianjently <vivianjently@gmail.com>
2025-04-18 16:45:24 +0100

    Added share button functionality to MemeFragment.java

--- app/src/main/java/com/example/cs4084_finalproject/MemeFragment.java
+++ app/src/main/java/com/example/cs4084_finalproject/MemeFragment.java
@@ -5,12 +5,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import com.android.volley.toolbox.ImageRequest;
+import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Environment;
+import androidx.core.content.FileProvider;
+import java.io.File;
+import java.io.FileOutputStream;
+
 
 public class MemeFragment extends Fragment {
     private static final String ARG_MEME_URL = "meme_url";
@@ -44,6 +54,8 @@ public class MemeFragment extends Fragment {
                 textView.setText("Error loading meme");
             }
         }
+        ImageButton shareButton = view.findViewById(R.id.btn_share);
+        shareButton.setOnClickListener(v -> shareMeme());
 
         return view;
     }
@@ -58,5 +70,31 @@ public class MemeFragment extends Fragment {
                 }
         );
         mySingleton.getInstance(requireContext()).addToRequestQueue(imageRequest);
+    }
+
+    public void shareMeme(){
+        BitmapDrawable drawable = (BitmapDrawable) featuredImage.getDrawable();
+        if (drawable == null) return;
+
+        Bitmap bitmap = drawable.getBitmap();
+
+        try {
+            File cachePath = new File(requireContext().getCacheDir(), "images");
+            cachePath.mkdirs();
+            File file = new File(cachePath, "meme.png");
+            FileOutputStream stream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            stream.close();
+
+            Uri contentUri = FileProvider.getUriForFile(requireContext(), "com.example.cs4084_finalproject.fileprovider", file);
+
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("image/png");
+            shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
+            shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            startActivity(Intent.createChooser(shareIntent, "Share meme using"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
