7794871e0faca98425162d105724a8d5d23ffb88
Vivianjently <vivianjently@gmail.com>
2025-04-24 17:37:48 +0100

    added share function to JokeFragment.java

--- app/src/main/java/com/example/cs4084_finalproject/JokeFragment.java
+++ app/src/main/java/com/example/cs4084_finalproject/JokeFragment.java
@@ -1,9 +1,11 @@
 package com.example.cs4084_finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -35,6 +37,22 @@ public class JokeFragment extends Fragment {
             jokeText.setText(joke);
         }
 
+
+        ImageButton share_button = view.findViewById(R.id.btn_share);
+        share_button.setOnClickListener(view1 -> shareJoke());
+
         return view;
+    }
+
+    public void shareJoke() {
+         String joke = jokeText.getText().toString();
+         if(joke.isEmpty()){
+             return;
+         }
+         Intent shareJoke_intent = new Intent(Intent.ACTION_SEND);
+         shareJoke_intent.setType("text/plain");
+         shareJoke_intent.putExtra(Intent.EXTRA_TEXT,joke);
+         startActivity(Intent.createChooser(shareJoke_intent, "Share this joke using"));
+
     }
 }
