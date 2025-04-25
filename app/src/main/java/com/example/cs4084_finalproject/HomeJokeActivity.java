3c9e20a275ba3b32d15fce29832eb73c1e36b63a
Michal Kornacki <22361979@studentmail.ul.ie>
2025-04-25 17:27:27 +0100

    cleaned up the code a lil by removing unused methods

--- app/src/main/java/com/example/cs4084_finalproject/HomeJokeActivity.java
+++ app/src/main/java/com/example/cs4084_finalproject/HomeJokeActivity.java
@@ -31,34 +31,6 @@ public class HomeJokeActivity extends AppCompatActivity {
     private RequestQueue requestQueue;
     private DBHandler dbHandler = new DBHandler(this);
 
-    /*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.top_bar, menu);
-        setTopBarTitle("Laughs");
-        Objects.requireNonNull(getSupportActionBar()).setHomeAsUpIndicator(R.drawable.ic_settings);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
-        return true;
-    }
-     */
-    /*
-    public void setTopBarTitle(String title){
-        TextView textView = new TextView(this);
-        textView.setText(title);
-        textView.setTextSize(20);
-        textView.setTypeface(null, Typeface.BOLD);
-        textView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(getResources().getColor(R.color.orange));
-        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setCustomView(textView);
-    }
-     */
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
